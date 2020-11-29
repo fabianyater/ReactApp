9c16116f8efe6073c4ca5315539d4cfdefd45d3f
Fabian Yate Ramirez <fabian_150299@hotmail.com>
2020-11-29 01:47:45 -0500

    Guardar nuevo comic

--- src/containers/Comics/Comics.js
+++ src/containers/Comics/Comics.js
@@ -4,11 +4,21 @@ import { Api } from "../../Common/api";
 import { Modal } from "antd";
 import "./Comics.css";
 
-
 const Comics = () => {
   const [comics, setComics] = useState([]);
   const [selected, setSelected] = useState({});
   const [visible, setVisible] = useState(false);
+  const [newComic, setNewComic] = useState([]);
+
+  const [inputs, setInputs] = useState({
+    id: "",
+    title: "",
+    description: "",
+    thumbnail: {
+      path: "",
+      extension: "",
+    },
+  });
 
   useEffect(() => {
     Api()
@@ -25,39 +35,73 @@ const Comics = () => {
   const handleOk = () => setVisible(false);
   const handleCancel = () => setVisible(false);
 
+  const onChange = (e) => {
+    if (e.target.name === "path" || e.target.name === "extension") {
+      setInputs({
+        ...inputs,
+        thumbnail: {
+          ...inputs.thumbnail,
+          [e.target.name]: e.target.value,
+        },
+      });
+    } else {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const save = () => {
+    setNewComic([...newComic, { ...inputs }]);
+  };
+
+  console.log(save);
+
   if (comics.length > 0) {
     return (
       <>
-        <div className="comics-title">
-          <h1>COMICS</h1>
-          <h1>COMIC SELECCIONADO</h1>
-        </div>
         <div className="comics-container">
           <div className="comics">
-            {comics.map((comic) => {
+            <h1>COMICS</h1>
+            {[...newComic, ...comics].map((comic) => {
               const { id, title, description, thumbnail } = comic;
               return (
                 <ComicCard
                   key={id}
                   id={id}
                   title={title}
-                  description={ description == null ? "Sin descripción" : description.slice(0, 250) + " ..." } 
-                  thumbnail={ thumbnail ? `${thumbnail.path}.${thumbnail.extension}` : null }
+                  description={
+                    description == null
+                      ? "Sin descripción"
+                      : description.slice(0, 250) + " ..."
+                  }
+                  thumbnail={
+                    thumbnail
+                      ? `${thumbnail.path}.${thumbnail.extension}`
+                      : null
+                  }
                   onSelect={handleSelected}
                 />
               );
             })}
             <Modal
               title={selected.title}
-              visible={visible}
+              //visible={visible}
               onOk={handleOk}
               onCancel={handleCancel}
             >
-              <img src={selected.thumbnail} alt="Marvel Comics" height="190px" width="150px"></img>
+              <img
+                src={selected.thumbnail}
+                alt="Marvel Comics"
+                height="190px"
+                width="150px"
+              ></img>
               <p>{selected.description}</p>
             </Modal>
           </div>
           <div className="comics-details">
+            <h1>COMICS SELECCIONADO</h1>
             <div className="selected-comics">
               <div>
                 <ComicCard
@@ -67,33 +111,42 @@ const Comics = () => {
                 />
               </div>
             </div>
-            <div className="comics-form">
-              <div className="form-group">
-                <label>Id: </label>
-                <input type="text" name="id" />
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <div className="comics-form">
+                <div className="form-group">
+                  <label>Id: </label>
+                  <input type="text" name="id" onChange={onChange} />
+                </div>
+                <div className="form-group">
+                  <label>Título: </label>
+                  <input type="text" name="title" onChange={onChange} />
+                </div>
+                <div className="form-group">
+                  <label>Descripción: </label>
+                  <input type="text" name="description" onChange={onChange} />
+                </div>
+                <div className="form-group">
+                  <label>Path: </label>
+                  <input type="text" name="path" onChange={onChange} />
+                </div>
+                <div className="form-group">
+                  <label>Extension: </label>
+                  <input type="text" name="extension" onChange={onChange} />
+                </div>
+                <button className="bton" onClick={save}></button>
               </div>
-              <div className="form-group">
-                <label>Título: </label>
-                <input type="text" name="title" />
-              </div>
-              <div className="form-group">
-                <label>Descripción: </label>
-                <input type="text" name="description" />
-              </div>
-              <div className="form-group">
-                <label>Imagen: </label>
-                <input type="text" name="image" />
-              </div>
-
-              <input type="submit" name="submit" />
-            </div>
+            </form>
           </div>
         </div>
       </>
     );
   }
 
-  return <div />;
+  return <div Hola />;
 };
 
 export default Comics;
